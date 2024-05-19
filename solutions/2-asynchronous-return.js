778d273218086f24b150da18a1556da57e8745b9
rrraumpanzer <ayylmao3713@gmail.com>
2024-05-19 18:52:15 +0300

    №2,№3 b r a in hu r ts

--- solutions/2-asynchronous-return.js
+++ solutions/2-asynchronous-return.js
@@ -1,5 +1,8 @@
 import fs from 'fs';
 
 // BEGIN
+const write = (fileName, writing, callback) => 
+    fs.writeFile(fileName, writing, 'utf-8', callback);
 
+export default write;
 // END
--- solutions/3-asynchronous-flow.js
+++ solutions/3-asynchronous-flow.js
@@ -1,5 +1,10 @@
 import fs from 'fs';
 
 // BEGIN
-
+export const compareFileSizes = (filePath1, filePath2, callback) =>
+    fs.stat(filePath1, (_err, file1_stats) => {
+        fs.stat(filePath2, (_err, file2_stats) => {
+            callback(null, Math.sign(file1_stats.size - file2_stats.size));
+        })
+    })
 // END
