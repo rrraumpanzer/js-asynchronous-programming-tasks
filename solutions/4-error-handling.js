5f965df65c31cfad691e82458ff2eaebe898d6b3
rrraumpanzer <ayylmao3713@gmail.com>
2024-05-21 14:02:16 +0300

    №4 uuhhhh

--- solutions/4-error-handling.js
+++ solutions/4-error-handling.js
@@ -1,5 +1,21 @@
 import fs from 'fs';
+import { isError } from 'lodash';
 
 // BEGIN
-
+export const move = (fileResourcePath, fileInsertPath, callback) => { 
+    fs.readFile(fileResourcePath, (error1, data) => { //открыта скобка для writeFile
+        if (isError(data) || error1)
+        { callback(error1); return; }
+        
+        fs.writeFile(fileInsertPath, data, (error2) => { //открыта скобка для unlink 
+            if (error2)
+            { callback(error2); return;}
+        
+            fs.unlink(fileResourcePath, (error3) => { 
+                callback(error3);
+                return; ///////guard expression
+            })  //закрыть скобку от unlink
+        }) //закрыть скобку для writeFile
+    }) //закрыть скобку для readFile
+} //закрыть скобку функции
 // END
