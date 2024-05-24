22f795e0f25cdb96c7e91917ad71ea9dcecaae3e
rrraumpanzer <ayylmao3713@gmail.com>
2024-05-24 17:20:49 +0300

    Sechs

--- solutions/6-timers.js
+++ solutions/6-timers.js
@@ -2,4 +2,38 @@ import fs from 'fs';
 
 // BEGIN
 
+const watch = (filepath, interval, callback) => {
+    let lastEdit; 
+    try 
+    {
+        lastEdit = fs.statSync(filepath).mtimeMs;
+    }
+    catch (err)
+    {
+        return callback(err);
+    }
+    
+    let stats;
+const timerId = setInterval(() => {
+    try 
+    {
+        stats = fs.statSync(filepath);
+    }
+    catch (err)
+    {
+        clearInterval(timerId);
+        return callback(err);
+    }
+    if (stats.mtimeMs !== lastEdit) 
+    {
+        lastEdit = stats.mtimeMs;
+        callback(null);
+    }
+    
+    }, interval);
+
+  return timerId;
+};
+
+export default watch;
 // END
