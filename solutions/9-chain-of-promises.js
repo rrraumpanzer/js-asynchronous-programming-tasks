2bce42b2bf0cae85e71178a92422193f09439dd8
rrraumpanzer <ayylmao3713@gmail.com>
2024-05-24 15:59:16 +0300

    999999999999999999

--- solutions/9-chain-of-promises.js
+++ solutions/9-chain-of-promises.js
@@ -1,5 +1,17 @@
 import fsp from 'fs/promises';
+import path from 'path';
 
 // BEGIN
-
+export const getTypes = (listOfPaths) => {
+    let promises_array = listOfPaths.map((path) => {
+        return fsp.stat(path)
+            .then((stat) => 
+            {
+                if (stat.isDirectory()) return 'directory'
+                else return 'file';
+            })
+            .catch(() => null);
+    })
+    return Promise.all(promises_array);
+}
 // END
