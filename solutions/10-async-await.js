a7debee6f4f46b81536855c4241683f1320ff741
rrraumpanzer <ayylmao3713@gmail.com>
2024-05-24 16:30:12 +0300

    10!!!

--- solutions/10-async-await.js
+++ solutions/10-async-await.js
@@ -1,5 +1,12 @@
+import { forEachSeries } from 'async';
 import fsp from 'fs/promises';
 
 // BEGIN
-
+export const exchange = async (file1, file2) => {
+    let promise1 = fsp.readFile(file1, 'utf-8');
+    let promise2 = fsp.readFile(file2, 'utf-8');
+    const [data1, data2] = await Promise.all([promise1, promise2]);
+    await fsp.writeFile(file1, data2);
+    await fsp.writeFile(file2, data1);
+}
 // END
